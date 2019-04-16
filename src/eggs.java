442a7061ce973971ddcec77e044cad5c4893200e
Sym Conger <acesilverryuu@gmail.com>
2019-04-16 13:21:48 -0500

    eggs

--- src/eggs.java
+++ src/eggs.java
@@ -5,11 +5,14 @@ public class eggs {
     public static void main (String[] args) {
 
         int userEggs;
+        double dozen;
+        double lastEggs;
         double oneDozen = 12;
-        final int dollars;
-        final int cents;
+        final double DOLLARS;
+  //      final int CENTS;
         double perDozen = 3.25;
         double singleEgg = 45;
+        final double TOTAL;
 
         Scanner keyboard = new Scanner (System.in);
 
@@ -17,7 +20,17 @@ public class eggs {
 
         userEggs = keyboard.nextInt ();
 
-//        dollars = ;
+        DOLLARS = userEggs * perDozen;
 
+        dozen = userEggs / 12;
+
+ //       CENTS =
+
+  //      TOTAL = DOLLARS + CENTS;
+
+        lastEggs = userEggs % oneDozen;
+
+        System.out.println("You ordered " + userEggs + " eggs. That's " + dozen + " dozen at $3.25 per dozen and " +
+                lastEggs + " loose eggs at 45 cents each for a total of $" + /*TOTAL*/  ".");
     }
 }
