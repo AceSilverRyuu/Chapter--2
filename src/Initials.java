c0fee0e47772ca95a9c6abd7903e9ac1fc11029c
Sym Conger <acesilverryuu@gmail.com>
2019-04-17 12:46:50 -0500

    initials

---
+++ src/Initials.java
@@ -0,0 +1,11 @@
+public class Initials {
+
+    public static void main (String[] args){
+
+        char fistInitial = 'S';
+        char middleInitial = 'A';
+        char lastInitial = 'C';
+
+        System.out.println ("My initials are, " + fistInitial + "." + middleInitial + "." + lastInitial +".");
+    }
+}
